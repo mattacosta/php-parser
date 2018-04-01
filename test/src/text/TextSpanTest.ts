e329a8e1ca014a8c47ba41d8c835e173c6a02a32
Matt Acosta <mattacosta@users.noreply.github.com>
2018-03-31 20:59:37 -0700

    Add TextSpan tests.

---
+++ test/src/text/TextSpanTest.ts
@@ -0,0 +1,187 @@
+/**
+ * Copyright 2017 Matt Acosta
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+'use strict';
+
+import * as assert from 'assert';
+
+import { TextSpan } from '../../../src/text/TextSpan';
+
+describe('TextSpan', function() {
+
+  const large  = new TextSpan(2, 6);  // 0 1 { 2 3 4 5 6 7 } 8 9
+  const medium = new TextSpan(3, 4);  // 0 1 2 { 3 4 5 6 } 7 8 9
+  const small  = new TextSpan(4, 2);  // 0 1 2 3 { 4 5 } 6 7 8 9
+
+  // Is all of span B in span A?
+  describe('#contains()', function() {
+    it('should contain an equivalent span', () => {
+      assert.equal(medium.contains(medium), true);
+    });
+    it('should not contain a larger span', () => {
+      assert.equal(medium.contains(large), false);
+    });
+    it('should contain a smaller span', () => {
+      assert.equal(medium.contains(small), true);
+    });
+
+    it('should not contain span before start', () => {
+      assert.equal(medium.contains(new TextSpan(2, 4)), false);
+    });
+    it('should not contain span after end', () => {
+      assert.equal(medium.contains(new TextSpan(4, 4)), false);
+    });
+    it('should not contain span ending at start', () => {
+      assert.equal(medium.contains(new TextSpan(0, 2)), false);
+    });
+    it('should not contain span starting at end', () => {
+      assert.equal(medium.contains(new TextSpan(8, 2)), false);
+    });
+    it('should not contain span ending before start', () => {
+      assert.equal(medium.contains(new TextSpan(0, 1)), false);
+    });
+    it('should not contain span starting after end', () => {
+      assert.equal(medium.contains(new TextSpan(9, 1)), false);
+    });
+  });
+
+  describe('#intersection()', function() {
+    it('should intersect with an equivalent span', () => {
+      assert.equal(medium.intersection(medium)!.equals(medium), true);
+    });
+    it('should intersect with a larger span', () => {
+      assert.equal(medium.intersection(large)!.equals(medium), true);
+    });
+    it('should intersect with a smaller span', () => {
+      assert.equal(medium.intersection(small)!.equals(small), true);
+    });
+
+    it('should intersect with span before start', () => {
+      assert.equal(medium.intersection(new TextSpan(2, 4))!.equals(new TextSpan(3, 3)), true);
+    });
+    it('should intersect with span after end', () => {
+      assert.equal(medium.intersection(new TextSpan(4, 4))!.equals(new TextSpan(4, 3)), true);
+    });
+    it('should intersect with span ending at start', () => {
+      assert.equal(medium.intersection(new TextSpan(1, 2))!.equals(new TextSpan(3, 0)), true);
+    });
+    it('should intersect with span starting at end', () => {
+      assert.equal(medium.intersection(new TextSpan(7, 2))!.equals(new TextSpan(7, 0)), true);
+    });
+    it('should not intersect with span ending before start', () => {
+      assert.strictEqual(medium.intersection(new TextSpan(0, 2)), null);
+    });
+    it('should not intersect with span starting after end', () => {
+      assert.strictEqual(medium.intersection(new TextSpan(8, 2)), null);
+    });
+  });
+
+  // Is part of span B in or adjacent to span A?
+  describe('#intersectsWith()', function() {
+    it('should intersect with an equivalent span', () => {
+      assert.equal(medium.intersectsWith(medium), true);
+    });
+    it('should intersect with a larger span', () => {
+      assert.equal(medium.intersectsWith(large), true);
+    });
+    it('should intersect with a smaller span', () => {
+      assert.equal(medium.intersectsWith(small), true);
+    });
+
+    it('should intersect with span before start', () => {
+      assert.equal(medium.intersectsWith(new TextSpan(2, 4)), true);
+    });
+    it('should intersect with span after end', () => {
+      assert.equal(medium.intersectsWith(new TextSpan(4, 4)), true);
+    });
+    it('should intersect with span ending at start', () => {
+      assert.equal(medium.intersectsWith(new TextSpan(1, 2)), true);
+    });
+    it('should intersect with span starting at end', () => {
+      assert.equal(medium.intersectsWith(new TextSpan(7, 2)), true);
+    });
+    it('should not intersect with span ending before start', () => {
+      assert.equal(medium.intersectsWith(new TextSpan(0, 2)), false);
+    });
+    it('should not intersect with span starting after end', () => {
+      assert.equal(medium.intersectsWith(new TextSpan(8, 2)), false);
+    });
+  });
+
+  describe('#overlap()', function() {
+    it('should overlap with an equivalent span', () => {
+      assert.equal(medium.overlap(medium)!.equals(medium), true);
+    });
+    it('should overlap with a larger span', () => {
+      assert.equal(medium.overlap(large)!.equals(medium), true);
+    });
+    it('should overlap with a smaller span', () => {
+      assert.equal(medium.overlap(small)!.equals(small), true);
+    });
+
+    it('should overlap with span before start', () => {
+      assert.equal(medium.overlap(new TextSpan(2, 4))!.equals(new TextSpan(3, 3)), true);
+    });
+    it('should overlap with span after end', () => {
+      assert.equal(medium.overlap(new TextSpan(4, 4))!.equals(new TextSpan(4, 3)), true);
+    });
+    it('should not overlap with span ending at start', () => {
+      assert.strictEqual(medium.overlap(new TextSpan(1, 2)), null);
+    });
+    it('should not overlap with span starting at end', () => {
+      assert.strictEqual(medium.overlap(new TextSpan(7, 2)), null);
+    });
+    it('should not overlap with span ending before start', () => {
+      assert.strictEqual(medium.overlap(new TextSpan(0, 2)), null);
+    });
+    it('should not overlap with span starting after end', () => {
+      assert.strictEqual(medium.overlap(new TextSpan(8, 2)), null);
+    });
+  });
+
+  // Is part of span B in span A?
+  describe('#overlapsWith()', function() {
+    it('should overlap with an equivalent span', () => {
+      assert.equal(medium.overlapsWith(medium), true);
+    });
+    it('should overlap with a larger span', () => {
+      assert.equal(medium.overlapsWith(large), true);
+    });
+    it('should overlap with a smaller span', () => {
+      assert.equal(medium.overlapsWith(small), true);
+    });
+
+    it('should overlap with span before start', () => {
+      assert.equal(medium.overlapsWith(new TextSpan(2, 4)), true);
+    });
+    it('should overlap with span after end', () => {
+      assert.equal(medium.overlapsWith(new TextSpan(4, 4)), true);
+    });
+    it('should not overlap with span ending at start', () => {
+      assert.equal(medium.overlapsWith(new TextSpan(1, 2)), false);
+    });
+    it('should not overlap with span starting at end', () => {
+      assert.equal(medium.overlapsWith(new TextSpan(7, 2)), false);
+    });
+    it('should not overlap with span ending before start', () => {
+      assert.equal(medium.overlapsWith(new TextSpan(0, 2)), false);
+    });
+    it('should not overlap with span starting after end', () => {
+      assert.equal(medium.overlapsWith(new TextSpan(8, 2)), false);
+    });
+  });
+
+});
